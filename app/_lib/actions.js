11715754d92786a2531ee0492cc02434b3c25c05
Bhekisile <bhekisilejozi@gmail.com>
2024-07-04 15:52:14 +0200

    Manual cache revalidation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,5 +1,6 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 
@@ -13,7 +14,7 @@ export async function updateGuest(formData) {
   if (!/^[a-zA-z0-9]{6,12}$/.test(nationalID))
     throw new Error("Please provide a valid national ID");
 
-  const updateData = {nationality, countryFlag, nationalID}
+  const updateData = { nationality, countryFlag, nationalID }
 
   const { data, error } = await supabase
     .from("guests")
@@ -21,6 +22,8 @@ export async function updateGuest(formData) {
     .eq("id", session.user.guestId);
 
   if (error) throw new Error("Guest could not be updated");
+
+  revalidatePath('/account/profile')
 }
 
 export async function signInAction() {
